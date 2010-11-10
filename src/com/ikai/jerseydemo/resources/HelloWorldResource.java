9a1e931e2f7766ef2a2dbb1069657b0aac086a02
Ikai Lan <ikai.lan@gmail.com>
2010-11-10 12:09:48 -0800

    Initial commit

---
+++ src/com/ikai/jerseydemo/resources/HelloWorldResource.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.ikai.jerseydemo.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * @author Ikai Lan
+ *
+ */
+
+// The Java class will be hosted at the URI path "/helloworld"
+@Path("/helloworld")
+public class HelloWorldResource {
+
+    // The Java method will process HTTP GET requests
+    @GET 
+    // The Java method will produce content identified by the MIME Media
+    // type "text/plain"
+    @Produces("text/plain")
+    public String getClichedMessage() {
+        // Return some cliched textual content
+        return "Hello World from Ikai's web service";
+    }
+}
