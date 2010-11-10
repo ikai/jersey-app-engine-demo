b1b7280cf3ef8f94804b6e4a56614d8704bd68d0
Ikai Lan <ikai.lan@gmail.com>
2010-11-10 12:29:47 -0800

    Adding a demo for DefaultValue

--- src/com/ikai/jerseydemo/resources/GreetingResource.java
+++ src/com/ikai/jerseydemo/resources/GreetingResource.java
@@ -3,10 +3,12 @@
  */
 package com.ikai.jerseydemo.resources;
 
+import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 
 /**
  * @author Ikai Lan
@@ -19,8 +21,11 @@ public class GreetingResource {
 	
 	@GET
 	@Produces("text/plain")
-	public String getGreeting(@PathParam("name") String name) {
-		return "Greetings, " + name + "!";
+	public String getGreeting(
+			@PathParam("name") String name,
+			@DefaultValue("Greetings") @QueryParam("greeting") String greeting) {
+
+		return greeting + ", " + name + "!";
 	}
 
 }
